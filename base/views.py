cc293ea314c94fe9d5dda63473e5618009bd775c
OIITuMuCT <vataman@live.ru>
2023-10-26 17:28:03 +0300

    Добавлена авторизация пользователя

--- base/views.py
+++ base/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required 
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -11,10 +17,41 @@ from .forms import RoomForm
 # ]
 
 
+def loginPage(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 def home(request):
     # return HttpResponse('Home page')    # меняем HttpResponse на render
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}                            # 2 ДОБАВИЛИ переменную контекст и удалили из рендер
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q)|       
+        Q(name__icontains=q) | 
+        Q(description__icontains=q)
+        )
+    
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}                            # 2 ДОБАВИЛИ переменную контекст и удалили из рендер
     return render(request, 'base/home.html', context) # 1 добавили словарь в рендер {'rooms': rooms}
 
 def room(request, pk):
@@ -22,6 +59,8 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+
+@login_required(login_url='login') 
 def createRoom(request):
     form = RoomForm()
     if request.method == "POST":
@@ -32,3 +71,28 @@ def createRoom(request):
     
     context = {'form': form }
     return render(request, 'base/room_form.html', context)
+
+@login_required(login_url='login')
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!!!')
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room )
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
